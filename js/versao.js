9242481ae9f5f4a20c374985cee9b7dfad9b70ea
Marisvaldo Carrijo <marisvaldo.carrijo@ibrowse-sds.com.br>
2025-06-04 16:28:49 -0300

    Ajustes das imagens - carrossel e criar versionamento

--- js/versao.js
+++ js/versao.js
@@ -1,4 +1,4 @@
-const versao = "V - 1.02";
+const versao = "1.02";
 const copyright = "© 2025 - Todos os direitos reservados.";
 const autor = "Marisvaldo Gonçalves Carrijo";
 const contatoAutor = "marisvaldo@gmail.com";
